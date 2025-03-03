a07b8da009edf676d8f2c307e9dc8ea19b6cef26
root <root@LAPTOP-R6AOU550.localdomain>
2025-03-03 15:42:20 +0100

    implement streamlit - "finished" product
    Det var ikke et krav at appen skulle være 100% færdigt og efter jeg har kæmpet med cluster plotting, så har jeg valgt at skrive det var en mulighed til forbedring eller supplement.
    
    Meget simpelt layout

--- main.py
+++ main.py
@@ -1,72 +1,41 @@
-import ast
-import asyncio
-from typing import Any
-
-from scipy.sparse import hstack
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cluster import KMeans
 import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.preprocessing import MultiLabelBinarizer
 
-def recommend_movies(
-    movie_title: str, 
-    movies_df: pd.DataFrame, 
-    combined_features: Any  # csr_array
-) -> pd.DataFrame:
-    # get the cluster id from the selected movie
-    cluster_id = movies_df[movies_df["title"] == movie_title]["cluster"].values[0]
-    input_movie_index = movies_df[movies_df["title"] == movie_title].index[0]
-    input_movie_vector = combined_features[input_movie_index].toarray()
-
-    # filter movies that belong to the same cluster as the selected movie    
-    recommended_movies = movies_df[movies_df["cluster"] == cluster_id]
-    # remove the selected movie from the recommended movies (obviously)
-    recommended_movies = recommended_movies[recommended_movies["title"] != movie_title]
-    recommended_vectors = combined_features[recommended_movies.index].toarray()
-
-    # get the similarity score between movies, so we can sort them
-    # by their similarity to the selected movie to get the best
-    # recommendations
-    similarities = cosine_similarity(input_movie_vector, recommended_vectors).flatten()
-    recommended_movies["similarity"] = similarities
-    sorted_recommendations = recommended_movies.sort_values(by="similarity", ascending=False)
-    
-    return sorted_recommendations[["title", "overview", "similarity"]]
+import streamlit as st
+import model
 
 def main() -> None:
-    # read the movies gotten from the excel file.
+    st.set_page_config(
+        page_title="Film anbefalingssystem",
+        layout="wide",
+    )
+    # load the dataset from .csv file
     movies_df = pd.read_csv("data/top_rated_movies_10000.csv")
-
-    # vectorize the description of the movies
-    tfidf = TfidfVectorizer()
-    overview_matrix = tfidf.fit_transform(movies_df["overview"])
-
-    # pandas list are weird where they are list[str] instead 
-    # of the list[list[int]], they are supposed to be - this is the fix
-    fixed_genre_ids = [ast.literal_eval(genre_list) for genre_list in movies_df["genre"]]
     
-    # transform the genre ids to binary values
-    mlb = MultiLabelBinarizer()
-    genre_matrix = mlb.fit_transform(fixed_genre_ids)
-
-    # combine all features and convert to csr_array
-    combined_features = hstack((overview_matrix, genre_matrix)).tocsr()
+    # load the model
+    kmeans, features = model.fit(movies_df)    # mutates movies_df in function
     
-    # initalize the kmeans cluster
-    kmeans = KMeans(n_clusters=10, random_state=42)
-    movies_df["cluster"] = kmeans.fit_predict(combined_features)
 
-    # examples ...
-    recommedation = recommend_movies("28 Days Later", movies_df, combined_features)
-    print("Recommended movies based on - 28 Days Later")
-    print(recommedation[:20])
+    search_input = st.text_input("Vælg en film, du kan lide.")
 
-    recommedation = recommend_movies("Creep", movies_df, combined_features)
-    print("Recommended movies based on - Creep")
-    print(recommedation[:20])
+    if not search_input:
+        return
+    
+    recommendations, cluster_plot = st.tabs(["Anbefalinger", "Cluster plot"])
 
-    # TODO: implement streamlit to visualize stuff
+    if not movies_df.isin([search_input]).any().any():
+        st.write(f"There is no movie named `{search_input}`")
+        return
+    
+    recommended_movies = model.get_movies_based_on_title(movies_df, search_input, features)
+    recommendations.dataframe(recommended_movies.head(20), use_container_width=True)
+
+    cluster_plot.write(
+        "Man kunne eventuelt lave et cluster plot, "
+        "hvor man kan se hvor den udvalgte film er "
+        "i den cluster den tilhører og dermed få "
+        "et bedre indblik på hvilke film er "
+        "tættest på, den man valgte."
+    )
 
 if __name__ == "__main__":
     main()
---
+++ model.py
@@ -0,0 +1,53 @@
+import ast
+from typing import Any
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.sparse import hstack
+from sklearn.preprocessing import MultiLabelBinarizer
+
+def fit(df: pd.DataFrame) -> tuple[KMeans, Any]:
+    # vectorize the description of the movies
+    tfidf = TfidfVectorizer()
+    overview_matrix = tfidf.fit_transform(df["overview"])
+
+    # pandas list are weird where they are list[str] instead 
+    # of the list[list[int]], they are supposed to be - this is the fix
+    fixed_genre_ids = [ast.literal_eval(genre_list) for genre_list in df["genre"]]
+    
+    # transform the genre ids to binary values
+    mlb = MultiLabelBinarizer()
+    genre_matrix = mlb.fit_transform(fixed_genre_ids)
+
+    # combine all features and convert to csr_array
+    combined_features = hstack((overview_matrix, genre_matrix)).tocsr()
+    
+    # initalize the kmeans cluster
+    kmeans = KMeans(n_clusters=10, random_state=42)
+    df["cluster"] = kmeans.fit_predict(combined_features)
+    
+    return (kmeans, combined_features)
+
+def get_movies_based_on_title(df: pd.DataFrame, title: str, features: Any) -> pd.DataFrame:
+    # get the cluster id from the selected movie
+    cluster_id = df[df["title"] == title]["cluster"].values[0]
+    input_movie_index = df[df["title"] == title].index[0]
+    input_movie_vector = features[input_movie_index].toarray()
+
+    # filter movies that belong to the same cluster as the selected movie    
+    recommended_movies = df[df["cluster"] == cluster_id]
+    # remove the selected movie from the recommended movies (obviously)
+    recommended_movies = recommended_movies[recommended_movies["title"] != title]
+    recommended_vectors = features[recommended_movies.index].toarray()
+
+    # get the similarity score between movies, so we can sort them
+    # by their similarity to the selected movie to get the best
+    # recommendations
+    similarities = cosine_similarity(input_movie_vector, recommended_vectors).flatten()
+    recommended_movies["similarity"] = similarities
+    recommended_movies["sort_method"] = similarities + (recommended_movies["rating"] * 0.03)
+    
+    recommended_movies.sort_values(by="sort_method", ascending=False, inplace=True)
+    
+    return recommended_movies[["title", "overview", "rating", "similarity"]]
