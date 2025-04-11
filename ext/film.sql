17290084ab8b40b442ac5c2c08dcb13dbb0ad3ad
Simon G. <45692977+jeenyuhs@users.noreply.github.com>
2025-04-11 13:31:28 +0200

    remove diagram and update sql

--- ext/film.sql
+++ ext/film.sql
@@ -1,207 +1,140 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1deb5ubuntu1
--- https://www.phpmyadmin.net/
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19  Distrib 10.11.11-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Vært: localhost:3306
--- Genereringstid: 27. 03 2025 kl. 00:05:43
--- Serverversion: 10.6.18-MariaDB-0ubuntu0.22.04.1
--- PHP-version: 8.1.2-1ubuntu2.20
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: oracle
+-- ------------------------------------------------------
+-- Server version	10.11.11-MariaDB-0ubuntu0.24.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `film`
+-- Table structure for table `actors`
 --
 
--- --------------------------------------------------------
-
---
--- Struktur-dump for tabellen `actors`
---
-
+DROP TABLE IF EXISTS `actors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `actors` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=51093 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Data dump for tabellen `actors`
+-- Table structure for table `cast`
 --
 
-INSERT INTO `actors` (`id`, `name`) VALUES
-(1, 'Jack Black'),
-(2, 'Jason Momoa');
-
--- --------------------------------------------------------
-
---
--- Struktur-dump for tabellen `cast`
---
-
+DROP TABLE IF EXISTS `cast`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cast` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `movie_id` int(11) NOT NULL,
   `actor_id` int(11) NOT NULL,
-  `character_name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `character_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=64002 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Data dump for tabellen `cast`
--- TEST DATA
+-- Table structure for table `movies`
 --
 
-INSERT INTO `cast` (`id`, `movie_id`, `actor_id`, `character_name`) VALUES
-(1, 1, 1, 'Steve'),
-(2, 1, 2, 'Garrett Garrison');
-
--- --------------------------------------------------------
-
---
--- Struktur-dump for tabellen `movies`
---
-
+DROP TABLE IF EXISTS `movies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `movies` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` tinytext NOT NULL,
   `description` text NOT NULL,
-  `release_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `release_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `genres` int(11) NOT NULL COMMENT 'Genres(IntFlag)',
+  `runtime` int(11) NOT NULL DEFAULT 0,
   `available_on` int(11) NOT NULL COMMENT 'Providers(IntFlag)',
-  `rating` float DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `rating` float DEFAULT 0,
+  `youtube_trailer_id` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2133 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Data dump for tabellen `movies`
--- TEST DATA
+-- Table structure for table `reviews`
 --
 
-INSERT INTO `movies` (`id`, `title`, `description`, `release_date`, `genres`, `available_on`, `rating`) VALUES
-(1, 'The Minecraft Movie', '', '2025-03-26 22:18:53', 0, 0, 9.4);
-
--- --------------------------------------------------------
-
---
--- Struktur-dump for tabellen `reviews`
---
-
+DROP TABLE IF EXISTS `reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `reviews` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `movie_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `title` tinytext NOT NULL,
   `description` text NOT NULL,
+  `rating` float NOT NULL DEFAULT 0,
   `likes` int(11) DEFAULT 0,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `movie_index` (`movie_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `series`
+--
+
+DROP TABLE IF EXISTS `series`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `series` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` tinytext NOT NULL,
+  `description` text NOT NULL,
+  `release_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `finished_at` datetime DEFAULT NULL,
+  `genres` int(11) NOT NULL COMMENT 'Genres(IntFlag)',
+  `available_on` int(11) NOT NULL COMMENT 'Providers(IntFlag)',
+  `rating` float DEFAULT 0,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Data dump for tabellen `reviews`
--- TEST DATA
+-- Table structure for table `users`
 --
 
-INSERT INTO `reviews` (`id`, `movie_id`, `user_id`, `title`, `description`, `likes`, `created_at`) VALUES
-(1, 1, 1, 'SPOILER FREE REVIEW', 'Everyone dies.', 0, '2025-03-22 10:26:42');
-
--- --------------------------------------------------------
-
---
--- Struktur-dump for tabellen `users`
---
-
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(25) NOT NULL,
   `display_name` varchar(25) NOT NULL,
-  `password_hash` varchar(255) NOT NULL COMMENT 'BCrypt hash'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `password_hash` varchar(255) NOT NULL COMMENT 'BCrypt hash',
+  `email` varchar(255) NOT NULL,
+  `birthday` date NOT NULL,
+  `providers` int(11) NOT NULL DEFAULT 0 COMMENT 'Providers(IntFlag)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Data dump for tabellen `users`
--- TEST DATA
---
-
-INSERT INTO `users` (`id`, `username`, `display_name`, `password_hash`) VALUES
-(1, 'admin', 'Admin', 'a');
-
---
--- Begrænsninger for dumpede tabeller
---
-
---
--- Indeks for tabel `actors`
---
-ALTER TABLE `actors`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks for tabel `cast`
---
-ALTER TABLE `cast`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks for tabel `movies`
---
-ALTER TABLE `movies`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indeks for tabel `reviews`
---
-ALTER TABLE `reviews`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `movie_index` (`movie_id`);
-
---
--- Indeks for tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `user_username` (`username`);
-
---
--- Brug ikke AUTO_INCREMENT for slettede tabeller
---
-
---
--- Tilføj AUTO_INCREMENT i tabel `actors`
---
-ALTER TABLE `actors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Tilføj AUTO_INCREMENT i tabel `cast`
---
-ALTER TABLE `cast`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Tilføj AUTO_INCREMENT i tabel `movies`
---
-ALTER TABLE `movies`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Tilføj AUTO_INCREMENT i tabel `reviews`
---
-ALTER TABLE `reviews`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Tilføj AUTO_INCREMENT i tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-11 13:31:10
